19ec1076479c5e7541dd1114d02f085e9c9819db
Adam Faulkner <adzfaulkner@hotmail.com>
2023-11-23 09:57:21 +0000

    login attempt fix

--- bin/envs.sh
+++ bin/envs.sh
@@ -4,4 +4,4 @@ cp ./frontend/.env.example ./frontend/.env.production
 
 REPLACED=$(sed "/^VITE_CLIENT_ID/s/'[^']*'/'$VITE_CLIENT_ID'/g" .env.production | sed "/^VITE_API_KEY/s/'[^']*'/'$VITE_API_KEY'/g" )
 
-echo "$REPLACED" > .env.production
+echo "$REPLACED" > ./frontend/.env.production
