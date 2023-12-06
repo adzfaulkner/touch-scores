0e472e347d931cb3629cee74d74b8a841d1fad7b
Adam Faulkner <adzfaulkner@hotmail.com>
2023-12-06 19:47:13 +0000

    added aws rum logging

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { AwsRum } from 'aws-rum-web'
 
-import type {SheetConfig} from '@/types'
+import type { AwsRumConfig } from 'aws-rum-web'
+import type { SheetConfig } from '@/types'
 
 import App from './App.vue'
 import router from './router'
@@ -16,6 +18,31 @@ import { getWS } from "@/support/websocket";
 import { useAuthenticationStore } from '@/stores/authentication'
 import { useFixtureStore } from '@/stores/fixture'
 import { useFilterStore } from '@/stores/filters'
+
+try {
+    const config: AwsRumConfig = {
+        sessionSampleRate: 1,
+        guestRoleArn: "arn:aws:iam::543174106112:role/RUM-Monitor-eu-west-2-543174106112-5896880981071-Unauth",
+        identityPoolId: "eu-west-2:93c11437-1077-486f-838c-1fb24cebed91",
+        endpoint: "https://dataplane.rum.eu-west-2.amazonaws.com",
+        telemetries: ["performance","errors","http"],
+        allowCookies: false,
+        enableXRay: false
+    };
+
+    const APPLICATION_ID: string = '4d3d9fff-1af0-49d0-966f-ec8fd3e92c12';
+    const APPLICATION_VERSION: string = '1.0.0';
+    const APPLICATION_REGION: string = 'eu-west-2';
+
+    new AwsRum(
+        APPLICATION_ID,
+        APPLICATION_VERSION,
+        APPLICATION_REGION,
+        config
+    );
+} catch (error) {
+    // Ignore errors thrown during CloudWatch RUM web client initialization
+}
 
 const pinia = createPinia()
 const app = createApp(App)
