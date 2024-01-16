72d833dbbf65d76759dbe5bd55010030ad3f87e3
Adam Faulkner <adzfaulkner@hotmail.com>
2024-01-16 18:09:59 +0000

    year fix

--- frontend/test/support/fixture/pivot-on-v.test.ts
+++ frontend/test/support/fixture/pivot-on-v.test.ts
@@ -12,7 +12,7 @@ const aggregatesTestLines: AggregateTestLine[] = [
     [
         nationals23,
         NATIONALS23_READ_FROM_CELL,
-        ['SATURDAY 29 APRIL 2023', 'SUNDAY 30 APRIL 2023', 'MONDAY 1 MAY 2023'],
+        ['SATURDAY 29 APRIL %year', 'SUNDAY 30 APRIL %year', 'MONDAY 1 MAY %year'],
         ["9:30","10:20","11:10","12:00","12:50","13:40","14:30","15:00","15:30","16:00","16:50","17:40","9:00","9:50","10:40","11:30","12:25","13:20","14:15","15:10","16:10","17:10","9:55","10:50","11:45","12:40","13:35","16:30"],
         ["FIELD 1","FIELD 2","FIELD 3","FIELD 4","FIELD 5","FIELD 6","FIELD 7","FIELD 8"]
     ],
@@ -29,6 +29,9 @@ describe('Aggregates raw data', () => {
 
         const it = pivotOnV(rd, readFromCell, null)
         it(dates, times, pitches, fixtures, teams, new Set<string>(), new Set<string>(), dateFixtureCount)
+
+        const now = new Date()
+        expectedDates = expectedDates.map(date => date.replace('%year', String(now.getFullYear())))
 
         assert.deepEqual(expectedDates, Array.from(dates.values()))
         assert.deepEqual(expectedTimes, Array.from(times.values()))
