a563939ea7a0668975071f36c7a503b01e0cd59a
Adam Faulkner <adzfaulkner@hotmail.com>
2024-10-13 14:54:21 +0100

    updated home score range

--- frontend/src/sheet-config.ts
+++ frontend/src/sheet-config.ts
@@ -2,14 +2,14 @@ import type { SheetConfig } from '@/types'
 
 const sheetConfigs: SheetConfig[] = [
   {
-    sheetId: '14osmgWjrMOo_5ptZYf-jYXb_DTA2rRjitFFIqY4_D5s',
+    sheetId: '1Y0ckxL8HvbFncwF1zpYLq_CMbshyUXTNCyU2IZnILwk',
     schedule: [
       {
-        date: '2024-08-24T00:00:00+00:00',
+        date: '2024-10-19T00:00:00+00:00',
         ranges: {
-          fixtures: 'Schedule!A14:R69',
-          fixturePitches: 'Schedule!13:13',
-          slotInfo: 'Schedule!B12:Y12',
+          fixtures: 'Schedule!A7:F50',
+          fixturePitches: 'Schedule!6:6',
+          slotInfo: 'Schedule!B5:F5',
           playOffSlotInfo: null,
           refAllocations: 'Ref Allocations!2:58',
         },
@@ -17,79 +17,12 @@ const sheetConfigs: SheetConfig[] = [
     ],
     standingRanges: [
       {
-        label: 'Div 1 Pool A',
-        range: 'Pool A!B12:W20',
-      },
-      {
-        label: 'Div 1 Pool B',
-        range: 'Pool B!B12:W20',
-      },
-      {
-        label: 'Div 1 Pool C',
-        range: 'Pool C!B12:W20',
-      },
-      {
-        label: 'Div 1 Pool D',
-        range: 'Pool D!B12:W20',
-      },
-      {
-        label: 'Div 2 Pool E',
-        range: 'Pool E!B12:W20',
-      },
-      {
-        label: 'Div 2 Pool F',
-        range: 'Pool F!B12:W20',
+        label: 'Pool Table',
+        range: 'Standings!A2:J9',
       },
     ],
-    competition: 'mixed_nts',
-    label: 'Mixed NTS 2024 Div 1/2 R3',
-  },
-  {
-    sheetId: '1Lyh7DhGXqnUXSGJnOcOSrEndukKpdWLFZTkaBqcIsF8',
-    schedule: [
-      {
-        date: '2024-08-25T00:00:00+00:00',
-        ranges: {
-          fixtures: 'Schedule!A14:R73',
-          fixturePitches: 'Schedule!A13:R13',
-          slotInfo: 'Schedule!B12:AE12',
-          playOffSlotInfo: null,
-          refAllocations: 'Ref Allocations!A2:Q62',
-        },
-      },
-    ],
-    standingRanges: [
-      {
-        label: 'Div 3 Pool A',
-        range: 'Pool A!B12:W20',
-      },
-      {
-        label: 'Div 3 Pool B',
-        range: 'Pool B!B12:W20',
-      },
-      {
-        label: 'Div 3 Pool C',
-        range: 'Pool C!B12:W20',
-      },
-      {
-        label: 'Div 3 Pool D',
-        range: 'Pool D!B12:W20',
-      },
-      {
-        label: 'Div 4 Pool E',
-        range: 'Pool E!B12:W20',
-      },
-      {
-        label: 'Div 4 Pool F',
-        range: 'Pool F!B12:W20',
-      },
-      {
-        label: 'Div 4 Pool G',
-        range: 'Pool G!B12:W20',
-      },
-    ],
-    competition: 'mixed_nts',
-    label: 'Mixed NTS 2024 Div 3/4 R3',
+    competition: 'seds',
+    label: 'SEDS 24/25 R1',
   },
 ]
 
